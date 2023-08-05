a5d6775b86614126f6b8d4cba95e170d941ae0bd
Yuya Kimura <yuya1508272003@gmail.com>
2023-08-05 22:33:48 +0900

    bladeの使用

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -2,26 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PracticeController extends Controller
 {
-    //
     public function sample()
     {
-        return response('practice');
+        return view('practice');
     }
 
     public function sample2()
     {
         $test = 'practice2';
-        return response($test);
+            return view('practice2', ['testParam' => $test]);
     }
 
     public function sample3()
     {
         $test = 'test';
-        return response($test);
+            return view('practice3', ['testParam' => $test]);
     }
-
 }
